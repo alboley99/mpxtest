d840f808a64c51e445bf22cd2d5bdc905d4ab058
Alec Hoffman <alecjhoffman@gmail.com>
2014-03-22 10:54:35 +0000

    Initial Commit

---
+++ MPXTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MPXTest
+//
+//  Created by Alec Hoffman on 22/03/2014.
+//  Copyright (c) 2014 Alec Hoffman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MPXTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MPXTest
+//
+//  Created by Alec Hoffman on 22/03/2014.
+//  Copyright (c) 2014 Alec Hoffman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
